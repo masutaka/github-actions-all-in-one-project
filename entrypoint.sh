ca9a40674533b7b711284c4ef0df0ee3f06a6221
Takashi Masuda <masutaka.net@gmail.com>
2019-02-03 22:54:16 +0900

    Revert "Refactor"
    This reverts commit 5446f6ba56e699591781e93cc252748cd9f389ea.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 
 KIND=$1
 ACTION=$(jq -r '.action' < "$GITHUB_EVENT_PATH")
-CURL_COMMON_OPTIONS="-s -u $GITHUB_ACTOR:$GITHUB_TOKEN --retry 3 \
--H 'Accept: application/vnd.github.inertia-preview+json'"
 
 if [ "$ACTION" != opened ]; then
   echo "This action was ignored. (ACTION: $ACTION)"
@@ -12,7 +10,8 @@ fi
 
 find_project_id() {
   _PROJECT_NUMBER=$1
-  _PROJECTS=$(curl "$CURL_COMMON_OPTIONS" -X GET \
+  _PROJECTS=$(curl -s -X GET -u "$GITHUB_ACTOR:$GITHUB_TOKEN" --retry 3 \
+		   -H 'Accept: application/vnd.github.inertia-preview+json' \
 		   "https://api.github.com/repos/$GITHUB_REPOSITORY/projects")
   _PROJECT_URL="https://github.com/$GITHUB_REPOSITORY/projects/$_PROJECT_NUMBER"
   echo "$_PROJECTS" | jq -r ".[] | select(.html_url == \"$_PROJECT_URL\").id"
@@ -22,7 +21,8 @@ find_project_id() {
 find_column_id() {
   _PROJECT_ID=$1
   _INITIAL_COLUMN_NAME=$2
-  _COLUMNS=$(curl "$CURL_COMMON_OPTIONS" -X GET \
+  _COLUMNS=$(curl -s -X GET -u "$GITHUB_ACTOR:$GITHUB_TOKEN" --retry 3 \
+		  -H 'Accept: application/vnd.github.inertia-preview+json' \
 		  "https://api.github.com/projects/$_PROJECT_ID/columns")
   echo "$_COLUMNS" | jq -r ".[] | select(.name == \"$_INITIAL_COLUMN_NAME\").id"
   unset _PROJECT_ID _INITIAL_COLUMN_NAME _COLUMNS
@@ -36,7 +36,8 @@ case "$KIND" in
     ISSUE_ID=$(jq -r '.issue.id' < "$GITHUB_EVENT_PATH")
 
     # Add this issue to the project column
-    curl "$CURL_COMMON_OPTIONS" -X POST \
+    curl -s -X POST -u "$GITHUB_ACTOR:$GITHUB_TOKEN" --retry 3 \
+	 -H 'Accept: application/vnd.github.inertia-preview+json' \
 	 -d "{\"content_type\": \"Issue\", \"content_id\": $ISSUE_ID}" \
 	 "https://api.github.com/projects/columns/$INITIAL_COLUMN_ID/cards"
     ;;
@@ -44,7 +45,8 @@ case "$KIND" in
     PULL_REQUEST_ID=$(jq -r '.pull_request.id' < "$GITHUB_EVENT_PATH")
 
     # Add this pull_request to the project column
-    curl "$CURL_COMMON_OPTIONS" -X POST \
+    curl -s -X POST -u "$GITHUB_ACTOR:$GITHUB_TOKEN" --retry 3 \
+	 -H 'Accept: application/vnd.github.inertia-preview+json' \
 	 -d "{\"content_type\": \"PullRequest\", \"content_id\": $PULL_REQUEST_ID}" \
 	 "https://api.github.com/projects/columns/$INITIAL_COLUMN_ID/cards"
     ;;
