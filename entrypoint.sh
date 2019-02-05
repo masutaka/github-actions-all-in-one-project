9ab66d7b36826ab26e950964a277fa098f45d8aa
Takashi Masuda <masutaka.net@gmail.com>
2019-02-05 18:59:01 +0900

    Fixup

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,9 @@ fi
 
 find_project_id() {
   _PROJECT_URL=$1
+  _ORG_NAME=$(echo "$_PROJECT_URL" | sed -e 's@https://github.com/orgs/\([^/]\+\)/projects/[0-9]\+@\1@')
 
-  if echo $_PROJECT_URL | grep -qF 'https://github.com/orgs/'; then
-    _ORG_NAME=$(echo $_PROJECT_URL | gsed -e 's@https://github.com/orgs/\([^/]\+\)/projects/[0-9]\+@\1@')
+  if [ "$_ORG_NAME" ]; then
     _ENDPOINT="https://api.github.com/orgs/$_ORG_NAME/projects"
   else
     _ENDPOINT="https://api.github.com/repos/$GITHUB_REPOSITORY/projects"
