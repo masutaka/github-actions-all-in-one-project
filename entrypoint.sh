4c321a513f4f05749a6b7aa2928e9402ae5227c6
Takashi Masuda <masutaka.net@gmail.com>
2019-02-06 00:55:53 +0900

    Enable to inject MY_GITHUB_TOKEN

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,17 +12,25 @@ if [ "$ACTION" != opened ]; then
   exit 0
 fi
 
-find_project_id() {
+get_org_name() {
   _PROJECT_URL="$1"
 
   if echo "$_PROJECT_URL" | grep -qF 'https://github.com/orgs/'; then
-    _ORG_NAME=$(echo "$_PROJECT_URL" | sed -e 's@https://github.com/orgs/\([^/]\+\)/projects/[0-9]\+@\1@')
+    echo "$_PROJECT_URL" | sed -e 's@https://github.com/orgs/\([^/]\+\)/projects/[0-9]\+@\1@'
+  fi
+}
+
+find_project_id() {
+  _ORG_NAME="$1"
+  _PROJECT_URL="$2"
+
+  if [ "$_ORG_NAME" ]; then
     _ENDPOINT="https://api.github.com/orgs/$_ORG_NAME/projects"
   else
     _ENDPOINT="https://api.github.com/repos/$GITHUB_REPOSITORY/projects"
   fi
 
-  _PROJECTS=$(curl -s -X GET -u "$GITHUB_ACTOR:$GITHUB_TOKEN" --retry 3 \
+  _PROJECTS=$(curl -s -X GET -u "$GITHUB_ACTOR:$TOKEN" --retry 3 \
 		   -H 'Accept: application/vnd.github.inertia-preview+json' \
 		   "$_ENDPOINT")
   echo "$_PROJECTS" | jq -r ".[] | select(.html_url == \"$_PROJECT_URL\").id"
@@ -32,14 +40,22 @@ find_project_id() {
 find_column_id() {
   _PROJECT_ID="$1"
   _INITIAL_COLUMN_NAME="$2"
-  _COLUMNS=$(curl -s -X GET -u "$GITHUB_ACTOR:$GITHUB_TOKEN" --retry 3 \
+  _COLUMNS=$(curl -s -X GET -u "$GITHUB_ACTOR:$TOKEN" --retry 3 \
 		  -H 'Accept: application/vnd.github.inertia-preview+json' \
 		  "https://api.github.com/projects/$_PROJECT_ID/columns")
   echo "$_COLUMNS" | jq -r ".[] | select(.name == \"$_INITIAL_COLUMN_NAME\").id"
   unset _PROJECT_ID _INITIAL_COLUMN_NAME _COLUMNS
 }
 
-PROJECT_ID=$(find_project_id "${PROJECT_URL:?<Error> required this environment variable}")
+ORG_NAME=$(get_org_name "${PROJECT_URL:?<Error> required this environment variable}")
+
+if [ "$ORG_NAME" ]; then
+  TOKEN="$MY_GITHUB_TOKEN" # It's User's personal access token. It should be secret.
+else
+  TOKEN="$GITHUB_TOKEN"    # GitHub sets. The scope in only the repository containing the workflow file.
+fi
+
+PROJECT_ID=$(find_project_id "$ORG_NAME" "$PROJECT_URL")
 INITIAL_COLUMN_ID=$(find_column_id "$PROJECT_ID" "${INITIAL_COLUMN_NAME:?<Error> required this environment variable}")
 
 case "$CONTENT_TYPE" in
@@ -47,7 +63,7 @@ case "$CONTENT_TYPE" in
     ISSUE_ID=$(jq -r '.issue.id' < "$GITHUB_EVENT_PATH")
 
     # Add this issue to the project column
-    curl -s -X POST -u "$GITHUB_ACTOR:$GITHUB_TOKEN" --retry 3 \
+    curl -s -X POST -u "$GITHUB_ACTOR:$TOKEN" --retry 3 \
 	 -H 'Accept: application/vnd.github.inertia-preview+json' \
 	 -d "{\"content_type\": \"Issue\", \"content_id\": $ISSUE_ID}" \
 	 "https://api.github.com/projects/columns/$INITIAL_COLUMN_ID/cards"
@@ -56,7 +72,7 @@ case "$CONTENT_TYPE" in
     PULL_REQUEST_ID=$(jq -r '.pull_request.id' < "$GITHUB_EVENT_PATH")
 
     # Add this pull_request to the project column
-    curl -s -X POST -u "$GITHUB_ACTOR:$GITHUB_TOKEN" --retry 3 \
+    curl -s -X POST -u "$GITHUB_ACTOR:$TOKEN" --retry 3 \
 	 -H 'Accept: application/vnd.github.inertia-preview+json' \
 	 -d "{\"content_type\": \"PullRequest\", \"content_id\": $PULL_REQUEST_ID}" \
 	 "https://api.github.com/projects/columns/$INITIAL_COLUMN_ID/cards"
